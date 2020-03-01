8eae75a0aa079b7b84e847cffd338ab2795b6d1d
Fernando Seliputra <fernando_seliputra@hotmail.com>
2020-03-01 17:12:22 +0100

    changed toolbar behaviour

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-ng build --prod --output-path docs --base-href portfolio
+ng build --prod --output-path docs --base-href /portfolio/
 ECHO cloning index html as 404 html
 copy ".\docs\index.html" ".\docs\404.html"
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,76 @@
-import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef, AfterViewInit, HostBinding, HostListener } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { fromEvent } from 'rxjs';
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  pairwise,
+  share,
+  throttleTime
+} from 'rxjs/operators';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  animations: [
+    trigger('toggle', [
+      state(
+        'hidden',
+        style({ opacity: 0, transform: 'translateY(-100%)' })
+      ),
+      state(
+        'visible',
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+  ])],
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   @ViewChild('sideNav') sideNav:MatSidenav;
   @ViewChild('aboutMe') aboutMe:ElementRef;
   @ViewChild('projects') projects:ElementRef;
   @ViewChild('technologies') technologies:ElementRef;
   @ViewChild('contact') contact:ElementRef;
 
+ isVisible = true;
+  currentActive: number;
+
   constructor() { }
+
+  ngAfterViewInit() {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+
+    const goingUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const goingDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    goingUp$.subscribe(() => (this.isVisible = true));
+    goingDown$.subscribe(() => (this.isVisible = false));
+  }
 
   checkSideNav() {
     if(this.sideNav !== undefined){
@@ -38,5 +95,23 @@ export class HomeComponent {
 
   scrollToContact(){
     this.contact.nativeElement.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
+  @HostListener('window:scroll')
+  checkOffsetTop() {
+    if (window.pageYOffset >= this.aboutMe.nativeElement.offsetTop &&
+      window.pageYOffset < this.projects.nativeElement.offsetTop) {
+      this.currentActive = 1;
+    } else if (window.pageYOffset >= this.projects.nativeElement.offsetTop
+       && window.pageYOffset < this.technologies.nativeElement.offsetTop) {
+      this.currentActive = 2;
+    } else if (window.pageYOffset >= this.technologies.nativeElement.offsetTop
+      && window.pageYOffset < this.contact.nativeElement.offsetTop) {
+      this.currentActive = 3;
+    } else if (window.pageYOffset >= this.contact.nativeElement.offsetTop) {
+      this.currentActive = 4;
+    } else {
+      this.currentActive = 0;
+    }
   }
 }
